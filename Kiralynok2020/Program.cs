fee7e5f137b13400319bcbe5a7d37cc8b44c9d30
guthember <guthember@gmail.com>
2020-09-16 08:35:04 +0200

    Tabla osztály létrehozása

--- Kiralynok2020/Program.cs
+++ Kiralynok2020/Program.cs
@@ -8,6 +8,23 @@ namespace Kiralynok2020
 {
     class Program
     {
+        class Tabla
+        {
+            private char[,] T = new char[8, 8];
+            private char UresCella;
+            private int UresOszlopokSzama;
+            private int UresSorokSzama;
+
+            public Tabla(char ch) 
+            {
+            }
+
+            public void Elhelyez() { }
+            public void FajbaIr() { }
+            public void Megjelenit() { }
+            public int UresOszlop() { }
+            public int UresSor() { }
+        }
         static void Main(string[] args)
         {
         }
