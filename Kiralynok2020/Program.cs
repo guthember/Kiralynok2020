793916622910efc65385c7a04f8dddcc7483bb50
guthember <guthember@gmail.com>
2020-09-16 09:03:07 +0200

    2. feladat Tabla konstruktor készítése

--- Kiralynok2020/Program.cs
+++ Kiralynok2020/Program.cs
@@ -10,23 +10,35 @@ namespace Kiralynok2020
     {
         class Tabla
         {
-            private char[,] T = new char[8, 8];
+            private char[,] T;
             private char UresCella;
             private int UresOszlopokSzama;
             private int UresSorokSzama;
 
             public Tabla(char ch) 
             {
+                T = new char[8, 8];
+                UresCella = ch;
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        T[i, j] = UresCella;
+                    }
+                }
             }
 
             public void Elhelyez() { }
             public void FajbaIr() { }
             public void Megjelenit() { }
-            public int UresOszlop() { }
-            public int UresSor() { }
+            public int UresOszlop() { return 0; }
+            public int UresSor() { return 0; }
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Királynők feladat");
+
+            Console.ReadKey();
         }
     }
 }
